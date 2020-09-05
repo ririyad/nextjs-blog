2ec53776511bba6f5a39616c5d511db3e2e3147e
ririyad <rasheduliriyad@gmail.com>
2020-09-05 11:19:17 +0600

    Adding styling

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,20 +1,22 @@
 import Link from 'next/link'
 import Head from 'next/head'
+import Layout from '../../components/layout'
 
 export default function FirstPost() {
     return (
-        <>
+        <Layout>
         <Head>
             <title>First Post</title>
         </Head>
         
         <h2>
+            <h1>First Post</h1>
             <Link href="/">
                 <a>Back to Home</a>
             </Link>
         </h2>
 
-        </>
+        </Layout>
     )
     
 }
